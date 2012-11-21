fe72abd5afc572e5bdf62ec4d02f45386faaf8b8
Mosky <mosky.tw@gmail.com>
2012-11-21 20:59:38 +0800

    add example

--- examples/test_enhanced.py
+++ examples/test_enhanced.py
@@ -23,3 +23,12 @@ if __name__ == '__main__':
     print '### Dump Data Again'
     print
     print enhancedyaml.dump(data, default_flow_style=False)
+
+    print '### Dump Objects Which Are Generated in Runtime'
+    print
+    e1 = Example()
+    e2 = Example()
+    e1.data = "I don't have `anchor`."
+    e2.data = "I don't have `anchor`, too."
+    es = [e1, e2, e2, e1]
+    print enhancedyaml.dump(es, default_flow_style=False)
